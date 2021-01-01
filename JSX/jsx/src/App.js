b47e5ebefd0d16523f85ba1fcfe9e01b89d3f7ac
Jung seo young <sju06071@gmail.com>
2021-01-01 20:48:59 +0900

    inline styling1

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -3,14 +3,13 @@ import './App.css'
 
 function App() {
   const name = '리액트';
-  const style = { 
-    //background-color는 backgroundColor와 같이 카멜 표기법으로 작성
+  
+  return (<div style = {{ 
     backgroundColor: 'black',
-    color: 'aqua',
+    color: 'violet',
     fontSize: '48px',
     fontWeight: 'bold',
     paddig: 16
-  }; //DOM요소에 스타일링을 적용할때는 '객체형태'로!!
-  return <div style = {style}>{name}</div>;
+  }}>{name}</div>);
 }                       
 export default App;
