143f84606bcae81df311bd011cccf4bc96779eae
Jung seo young <sju06071@gmail.com>
2021-01-01 16:40:48 +0900

    undefined

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from 'react'; 
+import './App.css'
 
 function App() {
-  const name = '리액트'; // && 사용해서 조건부 랜더링
-  return <div> {name === '리액트' && <h1>리액트입니다.</h1>} </div>;
-}                       //  참  && 참 -> 참
-                        // 거짓 && 참 -> 거짓, false를 랜더링 할때는 null처럼 아무것도 나타나지 않음
+  const name = undefined; 
+   // return name; 함수에서 undefined만 반환하는 상황은 X
+   return name || '값이 undefined입니다.'; // undefined 일때 사용할 값을 지정
+}                       
 export default App;
