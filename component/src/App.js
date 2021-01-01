1f4c1d0797de2b7fd22a3b43eb8cae11301d2bc3
Jung seo young <sju06071@gmail.com>
2021-01-01 22:56:01 +0900

    MyComponent

--- component/src/App.js
+++ component/src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
+import MyComponent from "./MyComponent"; // 컴포넌트 불러오기
 
-class App extends Component {
-  render() {
-    const name = "react";
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
---
+++ component/src/MyComponent.js
@@ -0,0 +1,14 @@
+/* 컴포넌트 생성 방법 
+    1. 파일 만들기
+    2. 코드 작성하기
+    3. 모듈 내보내기 및 불러오기 */
+
+import React from "react";
+
+//함수형 컴포넌트
+const MyComponent = () => {
+  // ( ) => { } 를 사용해서 함수를 만듦.
+  return <div>나의 새롭고 멋진 컴포넌트</div>;
+};
+
+export default MyComponent; //모듈 내보내기
