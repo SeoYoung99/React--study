d929abbd86138ebe9e083635219a2023d20cbbfd
Jung seo young <sju06071@gmail.com>
2021-01-01 16:44:24 +0900

    JSX안에서 undefined

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   const name = undefined; 
    // return name; 함수에서 undefined만 반환하는 상황은 X
-   return name || '값이 undefined입니다.'; // undefined 일때 사용할 값을 지정
+   // return name || '값이 undefined입니다.'; undefined 일때 사용할 값을 지정
+   return <div>{name || 'name값이 undefined입니다.'}</div>
 }                       
 export default App;
