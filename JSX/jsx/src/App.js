ffdb3b884b58900eebd5e3f956446890c7598356
Jung seo young <sju06071@gmail.com>
2021-01-01 21:12:14 +0900

    closing tag & 주석처리

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'; 
 import './App.css'
-//HTML에서는 css클래스를 사용할 때 class 속성 사용
-//JSX에서는 className으로 설정해야함.
+
 function App() {
   const name = '리액트';
-  
-  return <div className= "react">{name}</div>;
+  return(
+    <>
+      <div className="react" //시작태그에는 주석 가능
+      >{name}</div>
+      <input></input>
+      <input /> {/* <- self-closing 태그 */}
+    </>
+  );
 }                       
 export default App;
