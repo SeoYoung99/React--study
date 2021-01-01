a69b3b1276ab95a58044817da1fcd215f17f17a5
Jung seo young <sju06071@gmail.com>
2021-01-01 15:52:57 +0900

    Fragment기능 이용

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React,{ Fragment } from 'react';
 
 function App() {
   return (
-    <div>
+    <Fragment>
 
     <h1>리액트 안녕!</h1>
     <h2>잘 작동하니?</h2>
+    <h3>Fragment 이용</h3>
 
-    </div>
+    </Fragment>
   );
     
 }
