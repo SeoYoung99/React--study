095667334006f5a7d0e4e3887688a2d3c8c08637
Jung seo young <sju06071@gmail.com>
2021-01-02 02:11:56 +0900

    props

--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -1,14 +1,7 @@
-/* 컴포넌트 생성 방법 
-    1. 파일 만들기
-    2. 코드 작성하기
-    3. 모듈 내보내기 및 불러오기 */
-
 import React from "react";
 
-//함수형 컴포넌트
-const MyComponent = () => {
-  // ( ) => { } 를 사용해서 함수를 만듦.
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = (props) => {
+  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
 };
 
-export default MyComponent; //모듈 내보내기
+export default MyComponent;
