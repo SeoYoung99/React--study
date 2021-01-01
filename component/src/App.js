f085e8250db898ec2a59baffbad72b2b3187eb08
Jung seo young <sju06071@gmail.com>
2021-01-01 23:13:31 +0900

    MyComponent

--- component/src/App.js
+++ component/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import MyComponent from "./MyComponent"; // 컴포넌트 불러오기
 
+/* < 화살표 함수 > : 자신이 종속된 인스턴스를 가리킴, 가독성 up
+  functoin twice(value) {
+    return value * 2;
+  }
+  
+  const triple = (value) => value  * 3;
+ */
 const App = () => {
   return <MyComponent />;
 };
+
+/* 
+  function App() {
+    return <MyComponent />
+  }
+*/
 
 export default App;
