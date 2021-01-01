b6a3ba424171aaf6d0b3ea36620062a8fae4891c
Jung seo young <sju06071@gmail.com>
2021-01-01 15:59:56 +0900

    javascript 표현

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -1,16 +1,13 @@
-import React,{ Fragment } from 'react';
+import React from 'react';
 
 function App() {
+  const name = '리액트'; //자바스크립트 표현식 { }사용
   return (
-    <Fragment>
-
-    <h1>리액트 안녕!</h1>
-    <h2>잘 작동하니?</h2>
-    <h3>Fragment 이용</h3>
-
-    </Fragment>
+    <>
+      <h1>{name} 안녕!</h1> 
+      <h2>잘 작동하니?</h2>
+    </>
   );
-    
 }
 
 export default App;
