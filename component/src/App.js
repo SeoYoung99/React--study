ac1fcc8aec70d9b5d3a05aa4ca0efe39a0b54acf
Jung seo young <sju06071@gmail.com>
2021-01-02 02:38:25 +0900

    children

--- component/src/App.js
+++ component/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MyComponent from "./MyComponent"; // 컴포넌트 불러오기
 
 const App = () => {
-  return <MyComponent name="React" />;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const MyComponent = (props) => {
-  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+  return (
+    <div>
+      안녕하세요, 제 이름은 {props.name}입니다. <br />
+      children 값은 {props.children}
+      {/* App.js의 <MyComponent>태그 사이의 내용을 보여줌 */}
+      입니다.
+    </div>
+  );
 };
 
+MyComponent.defaultProps = {
+  name: "기본이름",
+};
 export default MyComponent;
