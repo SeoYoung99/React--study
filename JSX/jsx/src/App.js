6f4326300fb634f18e39b6e6f5229785f10d42da
Jung seo young <sju06071@gmail.com>
2021-01-01 21:29:35 +0900

    commit

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'; 
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const name = '리액트';
-  return(
-    <>
-      <div className="react" //시작태그에는 주석 가능
-      >{name}</div>
-      <input></input>
-      <input /> {/* <- self-closing 태그 */}
-    </>
+  const name = "리액트";
+  return (
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
   );
-}                       
+}
 export default App;
