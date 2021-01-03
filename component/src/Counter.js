ab7d48b70d3e041464dca6bfeec38b80c900be9e
Jung seo young <sju06071@gmail.com>
2021-01-04 03:28:02 +0900

    callback

--- component/src/Counter.js
+++ component/src/Counter.js
@@ -14,19 +14,16 @@ class Counter extends Component {
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            //onClick 은 버튼의props
-            //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
-
-            this.setState((prevState) => {
-              //동기적으로 업데이트를 하고 싶으면 객체 대신 함수 인자를 전달!!
-              //setState를 사용해서 state에 새로운 값을 넣음
-              return {
-                number: prevState.number + 1,
-              };
-            });
-            this.setState((prevState) => ({
-              number: prevState.number + 1,
-            })); //함수에서 바로 객체를 반환
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                //setState의 두번째 파라미터로 콜백함수를 등록
+                console.log("방금 setState가 호출되었습니다. ");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
