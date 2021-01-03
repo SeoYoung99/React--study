f38f0a2f619365a0ca96b933ebd423fb28181f22
Jung seo young <sju06071@gmail.com>
2021-01-04 02:31:40 +0900

    state

--- component/src/App.js
+++ component/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import MyComponent from "./MyComponent"; // 컴포넌트 불러오기
+import Counter from "./Counter"; // 컴포넌트 불러오기
 
 const App = () => {
-  return (
-    <MyComponent name="React" favoritenumber={1}>
-      {/* favoritenumber를 필수props로 설정했으므로 적어주어야 한다! */}
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
 };
 
 export default App;
---
+++ component/src/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    //state를 설정하는 constructor 메서드
+    super(props); //반드시 호출해야함
+    //현재 컴포넌트가 상속받고 있는 리액트의 Component클래스가 지닌 생성자함수를 호출
+    this.state = {
+      number: 0, //state의 초기값 설정, state는 객체 형식!
+    };
+  }
+  render() {
+    const { number } = this.state; //state를 조회
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button
+          onClick={() => {
+            //onClick 은 버튼의props
+            //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
+            this.setState({ number: number + 1 });
+            //setState를 사용해서 state에 새로운 값을 넣음
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+export default Counter;
