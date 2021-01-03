4169c74d5b28d6343d2e5dfe5bb6de6633ddfaaa
Jung seo young <sju06071@gmail.com>
2021-01-04 01:15:09 +0900

    isRequired

--- component/src/App.js
+++ component/src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import MyComponent from "./MyComponent"; // 컴포넌트 불러오기
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return (
+    <MyComponent name="React" favoritenumber={1}>
+      {/* favoritenumber를 필수props로 설정했으므로 적어주어야 한다! */}
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 //필수 props를 지정하거나 props의 타입을 지정할 때 사용
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoritenumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name} 입니다. <br />
       children 값은 {children} 입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoritenumber}입니다.
     </div>
   );
 };
@@ -17,6 +19,8 @@ MyComponent.defaultProps = {
 
 MyComponent.propTypes = {
   name: PropTypes.string,
+  favoritenumber: PropTypes.number.isRequired,
+  //isRequired를 이용해 필수props 설정
 };
 
 export default MyComponent;
