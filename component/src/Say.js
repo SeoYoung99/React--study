a73a91beb3874ec7f6522ac5bc94c51754a3f4e5
Jung seo young <sju06071@gmail.com>
2021-01-04 03:56:00 +0900

    useState1

--- component/src/Say.js
+++ component/src/Say.js
@@ -4,14 +4,26 @@ const Say = () => {
   const [message, setMessage] = useState(""); //useState의 인자에는 상태의 초기값을 넣어줌
   //useState함수를 호출하면 배열이 반환
   //( 첫번째 원소는 현재상태->message, 두번째 원소는 상태를 바꿔주는 함수->setMessage)
-  const onClickEnter = () => setMessage("안녕하세요");
-  const onClickLeave = () => setMessage("안녕히 가세요");
+  //const onClickEnter = () => setMessage("안녕하세요");
+  //const onClickLeave = () => setMessage("안녕히 가세요");
+
+  const [color, setColor] = useState("black");
 
   return (
     <div>
-      <button onClick={onClickEnter}>입장</button>
-      <button onClick={onClickLeave}>퇴장</button>
-      <h1>{message}</h1>
+      <button onClick={() => setMessage("안녕하세요")}>입장</button>
+      <button onClick={() => setMessage("안녕히 가세요")}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        빨간색
+      </button>
+      <button style={{ color: "green" }} onClick={() => setColor("green")}>
+        초록색
+      </button>
+      <button style={{ color: "blue" }} onClick={() => setColor("blue")}>
+        파랑색
+      </button>
     </div>
   );
 };
