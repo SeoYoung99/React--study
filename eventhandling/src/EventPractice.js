f247395b31a0244be7860638a626cce6e7f13225
Jung seo young <sju06071@gmail.com>
2021-01-04 19:26:31 +0900

    state에 input담기

--- eventhandling/src/EventPractice.js
+++ eventhandling/src/EventPractice.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "", //constructor없이 초기값 설정
+  };
   render() {
     return (
       <div>
@@ -9,10 +12,13 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해보세요"
+          value={this.state.message} //input의 value = state에 있는 값
           onChange={(e) => {
-            //onChange이벤트를 설정!!
-            console.log(e);
-            console.log(e.target.value); //값이 바뀔때마다 콘솔에 기록
+            // onChange이벤트를 설정!!
+            this.setState({
+              // state 업데이트
+              message: e.target.value, // state의 message를 inpur값으로
+            });
           }}
         />
       </div>
