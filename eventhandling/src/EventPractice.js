2ad9f8dd8e841e02996857f982a05204f6f4e678
Jung seo young <sju06071@gmail.com>
2021-01-04 21:11:08 +0900

    임의메서드

--- eventhandling/src/EventPractice.js
+++ eventhandling/src/EventPractice.js
@@ -4,6 +4,27 @@ class EventPractice extends Component {
   state = {
     message: "", //constructor없이 초기값 설정
   };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this); // 임의메서드가 이벤트로 등록되는 과정에서 메서드와 this가 끊어지므로
+    this.handleClick = this.handleClick.bind(this); // 메서드를 this에 바인딩
+  }
+
+  handleChange(e) {
+    //임의메서드
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick(e) {
+    //임의메서드
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
   render() {
     return (
       <div>
@@ -14,27 +35,9 @@ class EventPractice extends Component {
           placeholder="아무거나 입력해보세요"
           value={this.state.message} //input의 value = state에 있는 값
           //이거 왜 한거임...?
-          onChange={(e) => {
-            // onChange이벤트를 설정!!
-            this.setState({
-              // state 업데이트
-              message: e.target.value, // state의 message를 input값으로
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={
-            //클릭이벤트가 발생하면 현재comment값(message)을 메시지박스로 띄우고 다시 공백으로 설정
-            () => {
-              alert(this.state.message);
-              this.setState({
-                message: "",
-              });
-            }
-          }
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
