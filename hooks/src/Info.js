4d8c5bbe69d19cd8825290c54637a0dcb181054c
Jung seo young <sju06071@gmail.com>
2021-01-14 03:15:34 +0900

    custom Hooks

--- hooks/src/Info.js
+++ hooks/src/Info.js
@@ -1,5 +1,6 @@
-import React, { useReducer } from "react";
-
+import React from "react";
+import useInputs from "./useInputs";
+/*
 function reducer(state, action) {
   // 리듀서 함수
   return {
@@ -7,20 +8,19 @@ function reducer(state, action) {
     [action.name]: action.value, // [e.target.name]: e.target.value 와 유사
     // input중 name을 key값으로 써서 입력된 값을 state의 해당 key에 해당하는 곳에 업데이트
   };
-}
-
+}*/
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "", //기본값
   });
 
   const { name, nickname } = state; //객체
-
+  /*
   const onChange = (e) => {
     dispatch(e.target); //이벤트 객체가 지니고 있는 e.target값 자체를 액션값으로 사용
     //액션값을 받으면 reducer함수 호출
-  };
+  }; */
   return (
     <div>
       <div>
