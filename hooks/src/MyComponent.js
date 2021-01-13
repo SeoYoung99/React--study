92893c3ccfaa59ef9b4d2c7a351fbbdd3db05acb
Jung seo young <sju06071@gmail.com>
2021-01-14 02:40:39 +0900

    useRef - function type

--- hooks/src/MyComponent.js
+++ hooks/src/MyComponent.js
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 
-class MyComponent extends Component {
-  id = 1;
-  setId = (n) => {
-    this.id = n;
+const RefSample = () => {
+  const id = useRef(1);
+  //로컬변수의 초기값 설정
+  const setId = (n) => {
+    id.current = n; //로컬변수를 수정하는 함수
   };
-  printId = () => {
-    console.log(this.id);
+  const printId = () => {
+    console.log(id.current); //로컬변수 조회
   };
-  render() {
-    return <div>MyComponent</div>;
-  }
-}
-export default MyComponent;
+  return <div>refsample</div>;
+};
+export default RefSample;
