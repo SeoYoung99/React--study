a6e2226e3c5c2bcb2621779321f8fa2e6c77caea
Jung seo young <sju06071@gmail.com>
2021-01-14 02:10:04 +0900

    local var - class type

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,16 +1,8 @@
-import React from 'react';
+import React from "react";
+import Counter from "./Counter";
 
-  function App() {
-    return (
-    <div>
-      <h1>리액트 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-      <h3>
-        안뇽
-      </h3>
-    </div>
-    )
-  }
-
+const App = () => {
+  return <Counter role="연산">컴포넌트</Counter>;
+};
 
 export default App;
--- hooks/src/Counter.js
+++ hooks/src/Counter.js
@@ -1,9 +1,10 @@
 import React, { useReducer } from "react";
 
 function reducer(state, action) {
+  //1. 리듀서 함수 만들기
   switch (action.type) {
-    case "INCREMENT":
-      return { value: state.value + 1 };
+    case "INCREMENT": //action.type == 'INCREMENT'일때
+      return { value: state.value + 1 }; //state의 value에 +1
     case "DECREMENT":
       return { value: state.value - 1 };
     default:
@@ -13,13 +14,15 @@ function reducer(state, action) {
 
 const Counter = () => {
   const [state, dispatch] = useReducer(reducer, { value: 0 });
-
+  //   현재상태                                     기본값
+  //  dispatch: 함수안에 액션값을 파라미터로 넣어주면 리듀서 함수 호출
   return (
     <div>
       <p>
         현재 카운터 값은 <b>{state.value}</b>입니다.
       </p>
       <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      {/* dispatch에 파라미터로 액션값을 넣어줌 -> reducer함수에서 action type에 맞게 실행 */}
       <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
     </div>
   );
