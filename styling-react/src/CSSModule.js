506c14b77f418f82da7a8b72c22ce5eea2a3cad9
Jung seo young <sju06071@gmail.com>
2021-01-19 16:43:13 +0900

    css module

---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./CSSModule.module.css";
+const CSSModule = () => {
+  return (
+    <div className={styles.wrapper}>
+      {" "}
+      {/* 스타일 클래스를 적용하고 싶은 요소에 {styles.[클래스이름]} 형태로 전달 */}
+      안녕하세요, 저는 <span className="something">CSS Module</span>
+      {/* 글로벌은 그냥 넣어줌 */}
+    </div>
+  );
+};
+export default CSSModule;
