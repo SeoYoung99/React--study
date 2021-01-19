46c5fdfc8c4526e909abf4d6e87f7e18c7f7b10a
Jung seo young <sju06071@gmail.com>
2021-01-19 16:48:14 +0900

    Template Literal

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./CSSModule.module.css";
 const CSSModule = () => {
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
       {" "}
       {/* 스타일 클래스를 적용하고 싶은 요소에 {styles.[클래스이름]} 형태로 전달 */}
       안녕하세요, 저는 <span className="something">CSS Module</span>
