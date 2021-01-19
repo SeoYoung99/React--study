a85e69b90ab4cb194c9d41eab5fab1272fa626b8
Jung seo young <sju06071@gmail.com>
2021-01-20 01:12:32 +0900

    styled-components

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div>
-      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
