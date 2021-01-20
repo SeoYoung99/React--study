7055f5a04e61a5c362fcf6f0c7cd2b0582fae524
Jung seo young <sju06071@gmail.com>
2021-01-20 22:38:04 +0900

    TodoInsert value

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { MdAdd } from 'react-icons/md'; // {아이콘 이름}
+import { useState } from '../../node_modules/react/cjs/react.development';
+import { useCallback } from '../../node_modules/react/index';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+  const [value, setValue] = useState(''); //useState로 상태 정의
+
+  //onChange함수를 콜백함수로 정의
+  const onChange = useCallback((e) => {
+    setValue(e.target.value); //상태를 인풋에 입력받은 값으로 업데이트 해준다.
+  }, []);
   return (
     <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value} //초기값
+        onChange={onChange} //onChange함수로 핸들링
+      />
       <button type="submit">
         <MdAdd />
       </button>
