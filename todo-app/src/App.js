3257ac06b9160adc1250605323d146f49930af07
Jung seo young <sju06071@gmail.com>
2021-01-21 15:18:07 +0900

    createBulkTodos

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,29 +3,23 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    //초기값으로 객체 배열이 들어감, todos는 객체 배열!!
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기', //내용
-      checked: true, //완료여부
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+  //기본값에 파라미터로 함수 형태를 넣어주면 컴포넌트가 처음 렌더링될 때만 createBulkTodos함수 실행된다.
 
-  //고윳값으로 사용될 id
   //ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
