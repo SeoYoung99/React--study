3b9f2a4cc2435421408e63e2387455d5f599b9bd
Jung seo young <sju06071@gmail.com>
2021-01-27 17:53:57 +0900

    Browserouter

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowseRouter } from "react-router-dom";
+//프로젝트에 리액트 라우터를 적용
+//웹 애플리케이션에 HTML5의 History API를 사용하여 페이지를 새로고침하지 않고도 주소를 변경하고,
+//현재 주소에 관련된 정보를 props로 쉽게 조회하거나 사용할 수 있도록 해준다.
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowseRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowseRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
