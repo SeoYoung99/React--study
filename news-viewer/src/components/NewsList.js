01694ef4630c96782538c6bfc6692ff87926f852
Jung seo young <sju06071@gmail.com>
2021-02-03 00:30:58 +0900

    API

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsLsitBlock = styled.div`
   box-sizing: border-box;
@@ -14,24 +15,44 @@ const NewsLsitBlock = styled.div`
     padding-right: 1rem;
   }
 `;
-const sampleArticle = {
-  //예시로 만든 객체
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
 
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  // articles라는 상태 선언
+  const [loading, setLoading] = useState(false);
+  // loadinf이라는 상태 선언 (API요청이 대기중인지 판별)
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'http://newsapi.org/v2/top-headlines?country=kr&apiKey=66cd214b91f0456ba0db9077554010dd',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  //대기 중일 때
+  if (loading) {
+    return <NewsLsitBlock> 대기 중...</NewsLsitBlock>;
+  }
+  //아직 articles값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+  //articles값이 유효할 때
   return (
     <NewsLsitBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={articles.url} article={article} />
+      ))}
     </NewsLsitBlock>
   );
 };
