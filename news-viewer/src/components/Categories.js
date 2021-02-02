c1ea2fb79501dbabd00d3aa1e6ebfab25ef143ba
Jung seo young <sju06071@gmail.com>
2021-02-03 01:15:09 +0900

    category click

---
+++ news-viewer/src/components/Categories.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const categories = [
+  //categories라는 배열 안에 name과 text값이 들어가 있는 객체들을 넣어
+  //한글로 된 카테고리와 실제 카테고리 값을 연결시켜 줌
+  {
+    name: 'all', //실제 카테고리 값
+    text: '전체보기', //렌더링할 때 사용할 한글 카테고리
+  },
+  {
+    name: 'business',
+    text: '비즈니스',
+  },
+  {
+    name: 'entertainment',
+    text: '엔터테인먼트',
+  },
+  {
+    name: 'health',
+    text: '건강',
+  },
+  {
+    name: 'science',
+    text: '과학',
+  },
+  {
+    name: 'sports',
+    text: '스포츠',
+  },
+  {
+    name: 'technology',
+    text: '기술',
+  },
+];
+const CategoriesBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
+`;
+
+const Category = styled.div`
+  font-size: 1.125rem;
+  cursor: pointer;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.25rem;
+  &:hover {
+    color: #495057;
+  }
+
+  ${(props) =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+      &:hover {
+        color: #3bc9db;
+      }
+    `}
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+const Categories = ({ onSelect, category }) => {
+  //받아온 props
+  return (
+    <CategoriesBlock>
+      {categories.map((c) => (
+        <Category
+          key={c.name}
+          active={category === c.name}
+          onClick={() => onSelect(c.name)}
+        >
+          {c.text}
+        </Category>
+      ))}
+    </CategoriesBlock>
+  );
+};
+
+export default Categories;
