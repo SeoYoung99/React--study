885eed1659a7f54577e8d7c0d9b5dc54483d8d5f
Jung seo young <sju06071@gmail.com>
2021-02-03 02:47:45 +0900

    usePromise

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
-const NewsLsitBlock = styled.div`
+const NewsListBlock = styled.div`
   box-sizing: border-box;
   padding-bottom: 3rem;
   width: 768px;
@@ -17,50 +18,36 @@ const NewsLsitBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  // articles라는 상태 선언
-  const [loading, setLoading] = useState(false);
-  // loadinf이라는 상태 선언 (API요청이 대기중인지 판별)
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
+    );
+  }, [category]);
 
-  useEffect(() => {
-    //async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        //현재 category값이 무엇인지에 따라 요청할 주소가 동적으로 바뀜
-        //category 값이 all이면query 값을 공백으로 설정하고,
-        //all이 아니면 &category=카테고리 형태의 문자열을 만들도록 함
-        const response = await axios.get(
-          //데이터를 불러와서
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=66cd214b91f0456ba0db9077554010dd`,
-          //그리고 그 문자열을 주소에 포함
-        );
-        setArticles(response.data.articles); //articles에 업데이트
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [category]); //category값이 바뀔 때마다 뉴스를 새로 불러와야 하므로 []에 category 넣어줌
-
-  //대기 중일 때
+  // 대기중일 때
   if (loading) {
-    return <NewsLsitBlock> 대기 중...</NewsLsitBlock>;
+    return <NewsListBlock>대기중...</NewsListBlock>;
   }
-  //아직 articles값이 설정되지 않았을 때
-  if (!articles) {
-    //해당값이 현재 null인지 꼭! 검사해야함
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
-  //articles값이 유효할 때
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
   return (
-    <NewsLsitBlock>
+    <NewsListBlock>
       {articles.map((article) => (
-        <NewsItem key={articles.url} article={article} />
+        <NewsItem key={article.url} article={article} />
       ))}
-    </NewsLsitBlock>
+    </NewsListBlock>
   );
 };
+
 export default NewsList;
