c7342f2a1b4bf07929b30035485408e6467c5046
Jung seo young <sju06071@gmail.com>
2021-02-02 22:54:24 +0900

    async/ await

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
       //axios.get함수는 파라미터로 전달된 주소에 GET요청을 해주고, 이에 대한 결과는 .then을 통해 비동기적으로 확인
-      .then((response) => {
-        setData(response.data);
-      });
+
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
