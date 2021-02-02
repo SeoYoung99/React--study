e103aef9cb9c796c3b162c0a00dc2da3678639a9
Jung seo young <sju06071@gmail.com>
2021-02-02 23:03:49 +0900

    get news API

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=66cd214b91f0456ba0db9077554010dd',
       );
       //axios.get함수는 파라미터로 전달된 주소에 GET요청을 해주고, 이에 대한 결과는 .then을 통해 비동기적으로 확인
 
