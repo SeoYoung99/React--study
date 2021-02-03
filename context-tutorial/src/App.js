e45303aa7278053f5d14afd194ebdee7d1c7171d
Jung seo young <sju06071@gmail.com>
2021-02-04 01:46:17 +0900

    ColorBox

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+import { ColorProvider } from "./contexts/color";
 
 const App = () => {
   return (
-    <ColorContext.Provider
-      value={{ color: "red" }}
-      // provider를 쓸 때는 반드시 value값을 명시해야함
-    >
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 };
 export default App;
