3b82a2007bbd3e434157cb6fe5c009234b9afae8
Jung seo young <sju06071@gmail.com>
2021-02-04 10:46:22 +0900

    onClick & onContextMenu

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,5 +1,6 @@
 //Context의 actions에 넣어준 함수를 호출하는 컴포넌트를 만든다.
 import React from "react";
+import { ColorConsumer } from "../contexts/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
@@ -7,19 +8,30 @@ const SelectColors = () => {
   return (
     <div>
       <h2>색상을 선택하세요.</h2>
-      <div style={{ display: "flex" }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: "24px",
-              height: "24px",
-              cursor: "pointer",
-            }}
-          />
-        ))}
-      </div>
+      <ColorConsumer>
+        {({ actions }) => (
+          <div style={{ display: "flex" }}>
+            {colors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: "24px",
+                  height: "24px",
+                  cursor: "pointer",
+                }}
+                onClick={() => actions.setColor(color)}
+                onContextMenu={(e) => {
+                  //마우스 오른쪽 버튼 클릭
+                  e.preventDefault(); //마우스 오른쪽 버튼 클릭시 메뉴가 뜨는 것을 무시
+                  actions.setSubcolor(color);
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </ColorConsumer>
+      <hr />
     </div>
   );
 };
