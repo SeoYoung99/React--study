a3662b347009b53994ab1fb55cd8840ea302b733
Jung seo young <sju06071@gmail.com>
2021-02-04 12:35:00 +0900

    random color btn

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,50 +1,57 @@
-//Context의 actions에 넣어준 함수를 호출하는 컴포넌트를 만든다.
 import React, { Component } from "react";
 import ColorContext from "../contexts/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+//변경사항1
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class SelectColors extends Component {
-  static contextType = ColorContext; //contextType 사용하기
+  static contextType = ColorContext;
 
   handleSetColor = (color) => {
     this.context.actions.setColor(color);
   };
 
-  handleSetsubcolor = (subcolor) => {
-    this.context.actions.Setsubcolor(subcolor);
+  handleSetSubcolor = (subcolor) => {
+    this.context.actions.setSubcolor(subcolor);
+  };
+  //변경사항2
+  handleClick = (color) => {
+    //this.setState({
+    //  color: getRandomColor(),
+    //});
+    this.context.actions.setColor(getRandomColor());
   };
 
   render() {
     return (
       <div>
         <h2>색상을 선택하세요.</h2>
-
-        {({ actions }) => (
-          <div style={{ display: "flex" }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: "24px",
-                  height: "24px",
-                  cursor: "pointer",
-                }}
-                onClick={() => this.handleSetColor(color)}
-                onContextMenu={(e) => {
-                  //마우스 오른쪽 버튼 클릭
-                  e.preventDefault(); //마우스 오른쪽 버튼 클릭시 메뉴가 뜨는 것을 무시
-                  this.handleSetSubcolor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-
+        <div style={{ display: "flex" }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: "24px",
+                height: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubcolor(color);
+              }}
+            />
+          ))}
+          <button onClick={this.handleClick}>랜덤색상!</button>
+        </div>
         <hr />
       </div>
     );
   }
 }
+
 export default SelectColors;
