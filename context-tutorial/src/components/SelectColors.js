1f091f139462d25d75ca770a36544cb0b4f8c44e
Jung seo young <sju06071@gmail.com>
2021-02-04 10:56:45 +0900

    static contextType

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,14 +1,25 @@
 //Context의 actions에 넣어준 함수를 호출하는 컴포넌트를 만든다.
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { Component } from "react";
+import ColorContext from "../contexts/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-const SelectColors = () => {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
+class SelectColors extends Component {
+  static contextType = ColorContext; //contextType 사용하기
+
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSetsubcolor = (subcolor) => {
+    this.context.actions.Setsubcolor(subcolor);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+
         {({ actions }) => (
           <div style={{ display: "flex" }}>
             {colors.map((color) => (
@@ -20,19 +31,20 @@ const SelectColors = () => {
                   height: "24px",
                   cursor: "pointer",
                 }}
-                onClick={() => actions.setColor(color)}
+                onClick={() => this.handleSetColor(color)}
                 onContextMenu={(e) => {
                   //마우스 오른쪽 버튼 클릭
                   e.preventDefault(); //마우스 오른쪽 버튼 클릭시 메뉴가 뜨는 것을 무시
-                  actions.setSubcolor(color);
+                  this.handleSetSubcolor(color);
                 }}
               />
             ))}
           </div>
         )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
-};
+
+        <hr />
+      </div>
+    );
+  }
+}
 export default SelectColors;
