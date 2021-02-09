6983eae7c35b953ffe3b03b6c48fbf5a3752420b
Jung seo young <sju06071@gmail.com>
2021-02-10 01:55:52 +0900

    loggermiddleware

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,13 +1,17 @@
 //리덕스 적용하기
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import rootReducer from "./modules";
+//import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer); //루트 리듀서를 이용하여 스토어 생성
+const logger = createLogger(); //리덕스 logger 사용하기
+//리덕스 미들웨어를 스토어를 생성하는 과정에서 적용
+const store = createStore(rootReducer, applyMiddleware(logger)); //루트 리듀서를 이용하여 스토어 생성
 
 ReactDOM.render(
   //Provider 컴포넌트로 App컴포넌트를 감싸서 리덕스 적용
