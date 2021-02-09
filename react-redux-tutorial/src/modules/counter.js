8f0bd1eb76495128eba9c291045f0ed0d0a5b2c0
Jung seo young <sju06071@gmail.com>
2021-02-09 18:08:32 +0900

    redux-actions

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,18 +1,19 @@
+import { createAction, handleActions } from 'redux-actions';
 //액션 타입 정의 (대문자)
 //문자열: 모듈 이름/ 액션 이름
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-//액션 생성 함수 만들기
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+//액션 생성 함수 만들기 (리덕스 액션 라이브러리 함수 사용)
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //counter모듈의 초기 상태
 const initialState = {
   number: 0,
 };
 
-//리듀서 함수 만들기
+/*리듀서 함수 만들기
 function counter(state = initialState, action) {
   //현재 상태를 참조
   switch (action.type) {
@@ -28,5 +29,15 @@ function counter(state = initialState, action) {
     default:
       return state;
   }
-}
+}*/
+
+//리듀서 함수 간단하게 만들기
+const counter = handleActions(
+  {
+    //첫번째 파라미터에 각 액션에 대한 업데이트 함수
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState, //두번째 파라미터는 초기상태
+);
 export default counter; //함수 내보내기 (default는 한개만 내보낼 수 있다.)
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,10 +1,64 @@
+import { createAction, handleActions } from 'redux-actions';
 //액션 타입 정의
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; //인풋 값을 변경함
 const INSERT = 'todos/INSERT'; //새로운 todo를 등록함
 const TOGGLE = 'todos/TOGGLE'; //todo를 체크 /체크 해제함
 const REMOVE = 'todos/REMOVE'; //todo를 제거함
 
-//액션 생성 함수 만들기 (파라미터 필요)
+//액션 생성 함수 만들기 (파라미터 필요) -> payload: 액션에 필요한 추가 데이터,
+//createAction의 두번째 함수에 payload를 정의하는 함수를 따로 선언
+export const changeInput = createAction(CHANGE_INPUT, (input) => input); //파라미터를 그대로 반환, 생략 가능
+
+let id = 3;
+//두번째 파라미터에 text를 넣어주면 todo객체가 반환
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}));
+
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
+
+//초기 상태
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: false,
+    },
+  ],
+};
+
+const todos = handleActions(
+  {
+    //파라미터로 받아온 값을 action.id등으로 넣는것이 아니라 action.payload라는 이름을 공통적으로 사용한다.
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState,
+);
+/*
 export const changeInput = (input) => ({
   type: CHANGE_INPUT,
   input,
@@ -74,5 +128,5 @@ function todos(state = initialState, action) {
     default:
       return state;
   }
-}
+}*/
 export default todos;
