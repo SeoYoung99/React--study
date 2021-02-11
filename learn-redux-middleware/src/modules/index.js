480618d0fb2deda99c9ed6172361d92d92f9cacb
Jung seo young <sju06071@gmail.com>
2021-02-11 09:04:32 +0900

    thunk sample

--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import counter from "./counter"; // counter모듈 불러오기
+import sample from "./sample";
 
-const rootReducer = combineReducers({ counter }); // 루트 리듀서 생성
+const rootReducer = combineReducers({ counter, sample }); // 루트 리듀서 생성
 
 export default rootReducer;
