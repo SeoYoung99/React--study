bd05df67f141c3daae84135e4fae749d1f957650
Jung seo young <sju06071@gmail.com>
2021-02-11 09:22:40 +0900

    createRequestThunk

--- learn-redux-middleware/src/lib/api.js
+++ learn-redux-middleware/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getPost = (id) =>
-  axios.get("https://jsonplaceholder.typicode.com/posts/${id}");
+  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
 export const getUsers = (id) =>
-  axios.get("https://jsonplaceholder.typicode.com/users");
+  axios.get(`https://jsonplaceholder.typicode.com/users`);
