0fbac92a167045b5a68428bd9fd7f033b59058c5
Jung seo young <sju06071@gmail.com>
2021-02-11 09:30:01 +0900

    createRequestThunk

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,5 +1,7 @@
+import { createStore } from "redux";
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
 
 //액션 타입 선언, 한 요청당 세개를 만든다.
 const GET_POST = "sample/GET_POST";
@@ -12,7 +14,7 @@ const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 //thunk함수 생성
 //thunk함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치
-export const getPost = (id) => async (dispatch) => {
+/*export const getPost = (id) => async (dispatch) => {
   dispatch({ type: GET_POST }); //요청을 시작한 것을 알림
   try {
     const response = await api.getPost(id);
@@ -30,6 +32,7 @@ export const getPost = (id) => async (dispatch) => {
   }
 };
 
+
 export const getUsers = () => async (dispatch) => {
   dispatch({ type: GET_USERS }); //요청을 시작한 것을 알림
   try {
@@ -46,8 +49,10 @@ export const getUsers = () => async (dispatch) => {
     }); //에러 발생
     throw e; //에러를 조회할 수 있게 해줌
   }
-};
+};*/
 
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 //초기상태 선언
 //요청의 로딩 중 상태는 loading이라는 객체에서 관리한다.
 const initialState = {
