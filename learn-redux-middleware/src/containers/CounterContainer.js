df3861c0ae027789562eea781778a79d460d871f
Jung seo young <sju06071@gmail.com>
2021-02-11 11:17:39 +0900

    click

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,15 +1,21 @@
 //컨테이너 컴포넌트 ( 리덕스를 연동해서 props들을 Counter로 넘겨주기 )
 import React from "react";
 import { connect } from "react-redux"; //connect함수
-import { increaseAsync, decreaseAsync } from "../modules/counter"; //counter모듈 불러오기
+import { increaseAsync, decreaseAsync, clickAsync } from "../modules/counter"; //counter모듈 불러오기
 import Counter from "../components/Counter"; //Counter컴포넌트 불러오기
 
-const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
+const CounterContainer = ({
+  number,
+  increaseAsync,
+  decreaseAsync,
+  clickAsync,
+}) => {
   return (
     <Counter
       number={number}
       onIncrease={increaseAsync}
       onDecrease={decreaseAsync}
+      click={clickAsync}
     />
   );
 };
@@ -21,5 +27,6 @@ export default connect(
   {
     increaseAsync, //액션을 디스패치하는 함수들
     decreaseAsync,
+    clickAsync,
   }
 )(CounterContainer);
--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -3,9 +3,11 @@ import { createAction, handleActions } from "redux-actions";
 
 const INCREASE = "counter/INCREASE"; //액션 타입 정의
 const DECREASE = "counter/DECREASE";
+const CLICK = "counter/CLICK";
 
 export const increase = createAction(INCREASE); //액션 생성 함수
 export const decrease = createAction(DECREASE);
+export const click = createAction(CLICK);
 
 //redux-thunk는 액션 생성 함수에서 일반 액션 객체를 반환하는 대신에 함수를 반환한다.
 export const increaseAsync = () => (dispatch) => {
@@ -15,9 +17,16 @@ export const increaseAsync = () => (dispatch) => {
   }, 1000);
 };
 export const decreaseAsync = () => (dispatch) => {
+  //1초 뒤에 decrease 함수를 디스패치
   setTimeout(() => {
     dispatch(decrease());
   }, 1000);
+};
+export const clickAsync = () => (dispatch) => {
+  //3초 뒤에 click 함수를 디스패치
+  setTimeout(() => {
+    dispatch(click());
+  }, 3000);
 };
 
 const initialState = 0; //초기상태 (꼭 객체일 필요는 없다)
