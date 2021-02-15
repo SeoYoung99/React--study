6fb503f974f7d5d45a95c864ce33cdc1a33b844d
Jung seo young <sju06071@gmail.com>
2021-02-15 18:22:12 +0900

    Loadable

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,26 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { Suspense, useState } from "react";
+import React, { useState } from "react";
+import loadable from "@loadable/component";
 
-const SplitMe = React.lazy(() => import("./SplitMe"));
-// 컴포넌트를 렌더링하는 시점에서 비동기적으로 로딩
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>, //로딩 중에 보여줄 UI는 loadable함수에서 선언
+});
+//React.lazy 대신 loadable
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload(); //컴포넌트 미리 불러오기
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense
-          fallback={<div>loading...</div>}
-          //코드 스플리팅된 컴포넌트를 로딩, fallback을 통해 로딩이 끝나지 않았을 때 보여줄 UI설정
+        <p
+          onClick={onClick}
+          onMouseOver={onMouseOver}
+          //마우스를 올리기만 해도 로딩이 시작
         >
-          {visible && <SplitMe />}
-        </Suspense>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
