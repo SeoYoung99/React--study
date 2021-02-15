6d4011fb4c4d59e9fd85d1c6110384ef08680971
Jung seo young <sju06071@gmail.com>
2021-02-15 17:27:53 +0900

    state SplitMe

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,25 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-//import notify from "./notify";
+import React, { Component } from "react"; //클래스형 컴포넌트로 전환
 
-function App() {
-  const onClick = () => {
-    //notify(); 이건 notify 코드가 main 파일 안에 들어감.
-    import("./notify").then((result) => result.default());
-    //import를 함수로 사용하면 Promise를 반환한다.
-
-    //따로 import함수를 사용하면 파일을 따로 분리시켜서 저장.
-    //실제 함수가 필요한 지점에서 파일을 불러와 사용할 수 있다.
+class App extends Component {
+  state = {
+    SplitMe: null,
   };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!!</p>
-      </header>
-    </div>
-  );
+  handleClick = async () => {
+    //배누에서 SpliMe 컴포넌트를 불러와 state에 넣어줌
+    const loadModule = await import("./SplitMe");
+    this.setState({
+      SplitMe: loadModule.default,
+    });
+  };
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && (
+            <SplitMe
+            //SplitMe가 유효하다면 렌더링
+            />
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
